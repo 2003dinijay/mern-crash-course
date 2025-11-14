095cd16d80c34402c9770267d065822b0ba39dbd
Dinithi Nanayakkara <dinijayo@gmail.com>
2025-11-14 10:59:18 +0530

    feat:product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  timestamp: true //make sure you have createdAt, updatedAt fields on each document
+});
+
+//creating product model
+const Product = mongoose.model('Product', productSchema);
+
+//mongoose needs to add the capital letter for model names and make it plural for you
+//since we are using it in different files we need to export it
+//once we make a Product in the mongoDB it will be stored in products collection
+
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,27 @@ dotenv.config();
 const app = express();
 
 
-app.get('/products', (req, res) => {
-  res.send('List of products');
+app.post('/products', async (req, res) => {
+  const product= req.body; //user will send product data in the request body
+
+  //required fields validation
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ message: 'Please provide name, price, and image for the product.' });
+  }
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({success: true, data: newProduct });
+  } catch (error) {
+   console.error('Error saving product:', error);
+   res.status(500).json({ message: 'Server Error: Unable to save product.' });
+  }
 });
 
-const PORT = 5000;
-
 // CONNECT TO DB
+
+const PORT = 5000;
 app.listen(PORT, () => {
   connectDB();
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
